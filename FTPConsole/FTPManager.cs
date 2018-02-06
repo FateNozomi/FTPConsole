6a49ffc6eba3262640bac1f0573dcbcbb331de2b
Will <wailinnyu@gmail.com>
2018-02-06 17:52:21 +0800

    Initial commit

---
+++ FTPConsole/FTPManager.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTPConsole
+{
+    public class FTPManager
+    {
+        private readonly IPAddress _ipAddress;
+        private readonly ushort _port;
+        private readonly string _userName;
+        private readonly string _password;
+
+        public FTPManager(IPAddress ipAddress, ushort port, string userName, string password)
+        {
+            _ipAddress = ipAddress;
+            _port = port;
+            _userName = userName;
+            _password = password;
+
+            URI = new Uri("ftp://" + _ipAddress + ":" + _port);
+        }
+
+        public Uri URI { get; private set; }
+
+        public void Download(string remotePath, string localPath)
+        {
+            FtpWebRequest request = CreateRequest(remotePath, WebRequestMethods.Ftp.DownloadFile);
+
+            using (FtpWebResponse response = (FtpWebResponse)request.GetResponse())
+            {
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    using (FileStream localFileStream = new FileStream(localPath, FileMode.CreateNew))
+                    {
+                        responseStream.CopyTo(localFileStream);
+                    }
+                }
+            }
+        }
+
+        public void Upload(string localPath, string remotePath)
+        {
+            FtpWebRequest request = CreateRequest(remotePath, WebRequestMethods.Ftp.UploadFile);
+
+            StreamReader sourceStream = new StreamReader(localPath);
+            byte[] fileContents = Encoding.UTF8.GetBytes(sourceStream.ReadToEnd());
+            sourceStream.Close();
+
+            request.ContentLength = fileContents.Length;
+
+            using (Stream requestStream = request.GetRequestStream())
+            {
+                requestStream.Write(fileContents, 0, fileContents.Length);
+            }
+
+            using (FtpWebResponse response = (FtpWebResponse)request.GetResponse())
+            {
+                Console.WriteLine("Upload File Complete, status {0}", response.StatusDescription);
+            }
+        }
+
+        public string ListDirectory(string path)
+        {
+            FtpWebRequest request = CreateRequest(path, WebRequestMethods.Ftp.ListDirectory);
+
+            using (FtpWebResponse response = (FtpWebResponse)request.GetResponse())
+            {
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    using (StreamReader reader = new StreamReader(responseStream))
+                    {
+                        return reader.ReadToEnd();
+                    }
+                }
+            }
+        }
+
+        public string ListDirectoryDetails(string path)
+        {
+            FtpWebRequest request = CreateRequest(path, WebRequestMethods.Ftp.ListDirectoryDetails);
+
+            using (FtpWebResponse response = (FtpWebResponse)request.GetResponse())
+            {
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    using (StreamReader reader = new StreamReader(responseStream))
+                    {
+                        return reader.ReadToEnd();
+                    }
+                }
+            }
+        }
+
+        public bool DirectoryExists(string path)
+        {
+            try
+            {
+                FtpWebRequest request = CreateRequest(path, WebRequestMethods.Ftp.ListDirectory);
+                return request.GetResponse() != null;
+            }
+            catch (WebException)
+            {
+                return false;
+            }
+        }
+
+        public void CreateDirectory(string path)
+        {
+            FtpWebRequest request = CreateRequest(path, WebRequestMethods.Ftp.MakeDirectory);
+
+            using (FtpWebResponse responseStream = (FtpWebResponse)request.GetResponse())
+            {
+                Console.WriteLine(responseStream.StatusCode);
+            }
+        }
+
+        private FtpWebRequest CreateRequest(string remotePath, string ftpWebRequestMethod)
+        {
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(
+                new Uri(URI, remotePath));
+            request.Credentials = new NetworkCredential(_userName, _password);
+            request.Method = ftpWebRequestMethod;
+
+            return request;
+        }
+    }
+}
---
+++ FTPConsole/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTPConsole
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                DoWork();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void DoWork()
+        {
+            Console.WriteLine("Please enter the XML file path to upload:");
+            string localPath = Console.ReadLine();
+            UploadXML(localPath);
+        }
+
+        private static void UploadXML(string path)
+        {
+            // Example File Name: AA.1.123456789.01.xml
+            //                    [StripMarking].[StripNumber].[AmkorId].[DCC].xml
+
+            IPAddress ipAddress = new IPAddress(new byte[] { 127, 0, 0, 1 });
+            FTPManager ftpManager = new FTPManager(ipAddress, 21, "admin", "admin");
+
+            string fileName = Path.GetFileName(path);
+            List<string> stripMarkingFolders = GetStripMarkingFolders(fileName);
+
+            Console.WriteLine("Creating Parent Folder...");
+            string parentFolder = string.Empty;
+            foreach (string folder in stripMarkingFolders)
+            {
+                parentFolder += "/" + folder;
+
+                if (!ftpManager.DirectoryExists(parentFolder))
+                {
+                    ftpManager.CreateDirectory(parentFolder);
+                }
+                else
+                {
+                    Console.WriteLine("{0} folder already exist! Skipping folder creation...", parentFolder);
+                }
+            }
+
+            string remotePath = parentFolder + "/" + fileName;
+
+            Console.WriteLine("Uploading XML file to {0}", remotePath);
+            ftpManager.Upload(path, remotePath);
+        }
+
+        private static List<string> GetStripMarkingFolders(string fileName)
+        {
+            int startIndex = 0;
+            int lastIndex = fileName.IndexOf(".");
+            int length = lastIndex - startIndex;
+
+            string stripMarking = fileName.Substring(startIndex, length);
+
+            List<string> folders = new List<string>();
+
+            foreach (char folderName in stripMarking)
+            {
+
+                folders.Add(folderName.ToString());
+            }
+
+            return folders;
+        }
+    }
+}
